ba1be1ef07d0aa3fc0a415b40a28c9d9d73fb459
dean <dean@192.168.1.46>
2020-02-07 16:59:16 +0900

    add log

--- DiseaseSpecificNetwork/src/main/java/kr/ac/snu/HomeController.java
+++ DiseaseSpecificNetwork/src/main/java/kr/ac/snu/HomeController.java
@@ -25,6 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("aaaaWelcome home! The client locale is {}.", locale);
+		logger.info("testtest");
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
